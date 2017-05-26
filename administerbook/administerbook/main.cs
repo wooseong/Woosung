970e9e7366585b69bfaa98d83fa6f9390bf9f3d6
신우성 <lisa960310@naver.com>
2017-05-26 20:19:23 +0900

    no message

--- administerbook/administerbook/main.cs
+++ administerbook/administerbook/main.cs
@@ -11,6 +11,7 @@ namespace administerbook
         static void Main(string[] args)
         {
             Menu menu = new Menu();
+            Console.SetWindowSize(180,40);
             menu.MenuList();
             
         }
